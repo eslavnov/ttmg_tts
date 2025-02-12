ae820d013e02c30b26bbec2375e3d7ae795d6d02
Evgeny Slavnov <eslavnov@gmail.com>
2025-02-12 20:22:41 +0100

    Add support for local agent response

--- custom_components/ttmg_tts/tts.py
+++ custom_components/ttmg_tts/tts.py
@@ -1,104 +1,80 @@
+"""Support for the TTMG TTS service."""
+
 from __future__ import annotations
 
+import requests
 import logging
-from pathlib import Path
-from typing import Any, cast
+from typing import Any
+import json
 import voluptuous as vol
 
 from homeassistant.components.tts import (
-    CONF_LANG,
-    PLATFORM_SCHEMA as TTS_PLATFORM_SCHEMA,
     Provider,
     TextToSpeechEntity,
     TtsAudioType,
-    Voice,
 )
-from homeassistant.config_entries import SOURCE_IMPORT, ConfigEntry
-from homeassistant.core import HomeAssistant, callback
-from homeassistant.helpers import device_registry as dr
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
 from .const import (
-    DOMAIN,
     CONF_URL,
 )
 
 _LOGGER = logging.getLogger(__name__)
+
+
+async def async_get_engine(
+    hass: HomeAssistant,
+    config: ConfigType,
+    discovery_info: DiscoveryInfoType | None = None,
+) -> TTMGProvider:
+    """Set up TTMG TTS component."""
+    return TTMGProvider(hass)
+
 
 async def async_setup_entry(
     hass: HomeAssistant,
     config_entry: ConfigEntry,
     async_add_entities: AddEntitiesCallback,
 ) -> None:
-    async_add_entities(
-        [
-            TTMGTTSEntity(
-                config_entry,
-            )
-        ]
-    )
+    """Set up TTMG TTS platform via config entry."""
+    async_add_entities([TTMGTTSEntity(config_entry)])
 
 
-class BaseTTMGProvider:
+class TTMGTTSEntity(TextToSpeechEntity):
+    """The TTMG TTS API entity."""
 
-    def __init__(
-        self,
-        options_schema: vol.Schema,
-    ) -> None:
-        self._options_schema = options_schema
+    def __init__(self, config_entry: ConfigEntry) -> None:
+        """Init TTMG TTS service."""
+        self._attr_name = "TTMG TTS"
+        self._attr_unique_id = config_entry.entry_id
+        self._attr_default_language = "en-US"  
+        self._attr_supported_languages = ["en-US"]
 
-    @callback
-    def async_get_supported_voices(self, language: str) -> list[Voice] | None:
-        return None
-
-    async def _async_get_tts_audio(
-        self,
+    def get_tts_audio(
+        self, message: str,
     ) -> TtsAudioType:
+        """Load TTMG TTS"""
+        if message != "Processing your request, please wait...":
+          x = requests.post(CONF_URL+"/preload-text/ttmg_tts/", json={"text": json.dumps(message) }) 
         return "mp3", None
 
 
-class TTMGTTSEntity(BaseTTMGProvider, TextToSpeechEntity):
-  
-    def __init__(
-        self,
-        entry: ConfigEntry,
-    ) -> None:
-        self._attr_unique_id = f"{entry.entry_id}"
-        self._attr_name = entry.title
-        self._attr_device_info = dr.DeviceInfo(
-            identifiers={(DOMAIN, entry.entry_id)},
-            manufacturer="TTMG",
-            model="TTS",
-            entry_type=dr.DeviceEntryType.SERVICE,
-        )
-        self._attr_default_language = "en-US"  
-        self._attr_supported_languages = ["en-US"]
-        self._entry = entry
+class TTMGProvider(Provider):
+    """The TTMG TTS API provider."""
 
-    @property
-    def default_language(self) -> str:
-        return "en-US"
-      
-    @property
-    def supported_languages(self) -> list[str]:
-        return self._attr_supported_languages
-      
-    async def async_get_tts_audio(
-        self, 
-    ) -> TtsAudioType:
-        return None, None
-
-
-class TTMGTTSProvider(BaseTTMGProvider, Provider):
-    def __init__(
-        self,
-        options_schema: vol.Schema,
-    ) -> None:
-        super().__init__(options_schema)
+    def __init__(self, hass: HomeAssistant) -> None:
+        """Init TTMG TTS service."""
+        self.hass = hass
         self.name = "TTMG TTS"
 
-    async def async_get_tts_audio(
-        self,
+    def get_tts_audio(
+        self, message: str,
     ) -> TtsAudioType:
-        return None, None
+        """Load TTMG TTS"""
+        if message != "Processing your request, please wait...":
+          x = requests.post(CONF_URL+"/preload-text/ttmg_tts/", json={"text": json.dumps(message) }) 
+        return "mp3", None
